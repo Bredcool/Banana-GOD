d8662a445d08079c7ecd780008898f6c709db363
Rediawan Ibrahim <rediawanibrahim15@gmail.com>
2026-02-13 19:49:54 +0700

    update

--- frontend/src/data/menu.js
+++ frontend/src/data/menu.js
@@ -1,63 +1,64 @@
 export const menuList = [
     {
         id: 1,
-        name: "Choco Pharaoh",
+        name: "CHOCO PHARAOH",
         desc: "Balutan coklat penuh kemewahan",
         price: "Rp 18.000",
-        image: "/images/CHOCO PHARAOH.png"
+        image: "/images/CHOCO PHARAOH 2.png"
     },
-    // {
-    //     id: 2,
-    //     name: "Choco Pharaoh",
-    //     desc: "Pisang coklat pilihan raja Mesir",
-    //     price: "Rp 15.000"
-    // },
+    {
+        id: 2,
+        name: "SACRED TIRAMISU",
+        desc: "Krim tiramisu lembut pilihan para dewa",
+        price: "Rp 18.000",
+        image: "/images/SACRED TIRAMISU 1.png"
+    },
     // {
     //     id: 3,
     //     name: "Golden Banana",
     //     desc: "Balutan coklat kemuliaan kerajaan",
-    //     price: "Rp 15.000"
+    //     price: "Rp 18.000"
     // },
     // {
     //     id: 4,
     //     name: "Pyramid Bite (12 pcs)",
     //     desc: "Potongan kecil persembahan para dewa",
-    //     price: "Rp 15.000"
+    //     price: "Rp 18.000"
     // },
     // {
     //     id: 5,
     //     name: "Sacred Banana",
     //     desc: "Pisang terpilih dalam coklat pekat",
-    //     price: "Rp 15.000"
+    //     price: "Rp 18.000"
     // },
     // {
     //     id: 6,
     //     name: "Anubis Dark",
     //     desc: "Coklat hitam dengan rasa berani",
-    //     price: "Rp 15.000"
+    //     price: "Rp 18.000"
     // },
     // {
     //     id: 7,
     //     name: "Pharaoh Box",
     //     desc: "Satu kotak kemewahan Mesir kuno",
-    //     price: "Rp 15.000"
+    //     price: "Rp 18.000"
     // },
     // {
     //     id: 8,
     //     name: "Divine Choco",
     //     desc: "Gigitan coklat yang tak terlupakan",
-    //     price: "Rp 15.000"
+    //     price: "Rp 18.000"
     // },
     // {
     //     id: 9,
     //     name: "Temple Bite (12 pcs)",
     //     desc: "Ritual manis dalam setiap potongan",
-    //     price: "Rp 15.000"
+    //     price: "Rp 18.000"
     // },
     // {
     //     id: 10,
     //     name: "Royal Offering",
     //     desc: "Persembahan terbaik BANANA GOD",
-    //     price: "Rp 15.000"
+    //     price: "Rp 18.000"
     // }
 ]
